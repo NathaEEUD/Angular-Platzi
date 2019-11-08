5a5823be752591e8036b6061daed99935a8328f5
Nathaly <nriano@robotec.com>
2019-11-08 09:06:18 -0500

    feat: adding the product component

---
+++ platzi-store/src/app/components/product/product.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+  product: IProduct = {
+    id: '1',
+    image: 'assets/images/camiseta.png',
+    title: 'Camiseta',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
