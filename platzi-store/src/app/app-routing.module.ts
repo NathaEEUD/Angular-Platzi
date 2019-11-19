abe085023a815c3f8bd9285cad552c8b4ab12297
Nathaly <nriano@robotec.com>
2019-11-19 11:02:09 -0500

    feat: implementing nested views as a good practice

--- platzi-store/src/app/app-routing.module.ts
+++ platzi-store/src/app/app-routing.module.ts
@@ -6,32 +6,40 @@ import { ContactComponent } from './components/contact/contact.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent
+      }
+    ]
   },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'products',
-    component: ProductsComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent
-  },
+
   {
     path: 'demo',
     component: DemoComponent
-  },
-  {
-    path: 'products/:id',
-    component: ProductDetailComponent
   },
   {
     path: '**',
