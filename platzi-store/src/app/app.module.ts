cbc7b01e32d88fc105a1aa4f0ca24f787f502218
Nathaly <nriano@robotec.com>
2019-11-19 09:55:08 -0500

    feat: using routerLink and routerLinkActive

--- platzi-store/src/app/app.module.ts
+++ platzi-store/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { DemoComponent } from './components/demo/demo.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { BannerComponent } from './components/banner/banner.component';
     DemoComponent,
     HeaderComponent,
     FooterComponent,
-    BannerComponent
+    BannerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
