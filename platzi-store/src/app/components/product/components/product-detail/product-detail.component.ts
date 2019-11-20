ca1049b91f23a9e740a69d721084359fef976ce6
Nathaly <nriano@robotec.com>
2019-11-20 16:36:50 -0500

    feat: implementing CRUD methods of products

--- platzi-store/src/app/components/product/components/product-detail/product-detail.component.ts
+++ platzi-store/src/app/components/product/components/product-detail/product-detail.component.ts
@@ -26,4 +26,35 @@ export class ProductDetailComponent implements OnInit {
       console.log('product::::', product);
     });
   }
+
+  createProduct() {
+    const newProduct: IProduct = {
+      id: '7',
+      title: 'New product from angular',
+      image: 'assets/images/banner-1.jpg',
+      description: 'New product',
+      price: 31000
+    };
+
+    this.productsService.createProduct(newProduct).subscribe(product => {
+      console.log('new product::::', product);
+    });
+  }
+
+  updateProduct() {
+    const editedProduct: Partial<IProduct> = {
+      description: 'Edited description',
+      price: 31000
+    };
+
+    this.productsService.updateProduct('7', editedProduct).subscribe(product => {
+      console.log('update product::::', product);
+    });
+  }
+
+  deleteProduct() {
+    this.productsService.deleteProduct('7').subscribe(product => {
+      console.log('delete product::::', product);
+    });
+  }
 }
--- platzi-store/src/app/core/services/products.service.ts
+++ platzi-store/src/app/core/services/products.service.ts
@@ -18,4 +18,16 @@ export class ProductsService {
   getProduct(id: string) {
     return this.http.get<IProduct>(environment.url_api + id);
   }
+
+  createProduct(product: IProduct) {
+    return this.http.post(environment.url_api, product);
+  }
+
+  updateProduct(id: string, editedProduct: Partial<IProduct>) {
+    return this.http.put(environment.url_api + id, editedProduct);
+  }
+
+  deleteProduct(id: string) {
+    return this.http.delete<IProduct>(environment.url_api + id);
+  }
 }
