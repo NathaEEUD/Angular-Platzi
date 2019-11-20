5267a513b70da847a9c8016b2dc71038d6625368
Nathaly <nriano@robotec.com>
2019-11-20 11:43:26 -0500

    feat: produts styles

--- platzi-store/src/app/material/material.module.ts
+++ platzi-store/src/app/material/material.module.ts
@@ -4,10 +4,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule, MatToolbarModule, MatIconModule, MatBadgeModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatBadgeModule]
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule
+  ],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatBadgeModule, MatCardModule]
 })
 export class MaterialModule {}
