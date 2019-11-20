c24ac60659357212d7ba776f411055c86eed5ada
Nathaly <nriano@robotec.com>
2019-11-20 14:49:44 -0500

    feat: moving variables to environment file

--- platzi-store/src/app/core/services/products.service.ts
+++ platzi-store/src/app/core/services/products.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from '../models/product.model';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  private URL = 'http://platzi-store.herokuapp.com/products/';
   products: IProduct[];
 
   constructor(private http: HttpClient) {}
 
   getAllProducts() {
-    return this.http.get<IProduct[]>(this.URL);
+    return this.http.get<IProduct[]>(environment.url_api);
   }
 
   getProduct(id: string) {
-    return this.http.get<IProduct>(this.URL + id);
+    return this.http.get<IProduct>(environment.url_api + id);
   }
 }
