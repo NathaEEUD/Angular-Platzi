f0cee729a8024d461f2361fcbea4582898a88448
Nathaly <nriano@robotec.com>
2019-11-26 16:37:38 -0500

    feat: adding custom form validations

--- platzi-store/src/app/admin/components/form-product/form-product.component.ts
+++ platzi-store/src/app/admin/components/form-product/form-product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductsService } from 'src/app/core/services/products.service';
 import { Router } from '@angular/router';
+import { MyValidators } from 'src/app/utils/validators';
 
 @Component({
   selector: 'app-form-product',
@@ -34,11 +35,15 @@ export class FormProductComponent implements OnInit {
     }
   }
 
+  get priceField() {
+    return this.form.get('price');
+  }
+
   private buildForm() {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
-      price: [0, [Validators.required]],
+      price: [0, [Validators.required, MyValidators.isPriceValid]],
       image: [''],
       description: ['', [Validators.required]]
     });
