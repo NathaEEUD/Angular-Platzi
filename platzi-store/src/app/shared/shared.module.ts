2d882b3018858cc45544b869ddd6b81d08a86e3f
Nathaly <nriano@robotec.com>
2019-11-26 12:02:17 -0500

    feat: introduction to formControl

--- platzi-store/src/app/shared/shared.module.ts
+++ platzi-store/src/app/shared/shared.module.ts
@@ -6,10 +6,11 @@ import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, ExponentialPipe, HighlightDirective],
-  imports: [CommonModule, RouterModule, MaterialModule],
+  imports: [CommonModule, RouterModule, MaterialModule, ReactiveFormsModule],
   exports: [HeaderComponent, FooterComponent, ExponentialPipe, HighlightDirective]
 })
 export class SharedModule {}
