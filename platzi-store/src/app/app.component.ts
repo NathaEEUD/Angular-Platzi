d569289ce88e2589947e7dfebf6a532d233d8034
Nathaly <nriano@robotec.com>
2019-11-29 15:23:50 -0500

    feat: adding google analytics

--- platzi-store/src/app/app.component.ts
+++ platzi-store/src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
-import { IProduct } from './core/models/product.model';
+// import { Router, NavigationEnd } from '@angular/router';
+// import { filter } from 'rxjs/operators';
+
+// declare var gtag;
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  // constructor(private router: Router) {
+  //   const navEndEvents$ = router.events.pipe(filter(event => event instanceof NavigationEnd));
+  //   navEndEvents$.subscribe((event: NavigationEnd) => {
+  //     gtag('config', 'UA-147051588-1', {
+  //       page_path: event.urlAfterRedirects
+  //     });
+  //   })
+  // }
+}
