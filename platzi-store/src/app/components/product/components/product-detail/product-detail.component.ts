64bcd56ac0205f83519c51656ea954b3ce9bcff2
Nathaly <nriano@robotec.com>
2019-12-05 09:35:35 -0500

    fix: avoiding double subscribe with Observables and switchMap

--- platzi-store/src/app/components/product/components/product-detail/product-detail.component.ts
+++ platzi-store/src/app/components/product/components/product-detail/product-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ProductsService } from '@core/services/products/products.service';
 import { IProduct } from '@core/models/product.model';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,22 +11,16 @@ import { IProduct } from '@core/models/product.model';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
-  product: IProduct;
+  product$: Observable<IProduct>;
 
   constructor(private route: ActivatedRoute, private productsService: ProductsService) {}
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      const { id } = params;
-      this.fetchProduct(id);
-    });
-  }
-
-  fetchProduct(id: string) {
-    this.productsService.getProduct(id).subscribe(product => {
-      this.product = product;
-      console.log('product::::', product);
-    });
+    this.product$ = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.productsService.getProduct(params.id);
+      })
+    );
   }
 
   createProduct() {
